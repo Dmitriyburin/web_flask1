8c4169adae6eb22acdfa8c49863fa98c756766f7
sovun1a <burindima324@gmail.com>
2022-02-09 16:53:47 +0300

    7

--- Результаты отборов/application.py
+++ Результаты отборов/application.py
@@ -10,7 +10,8 @@ def form_sample(nickname, level: int, rating: float):
     url_style = url_for('static', filename='css/style.css')
 
     if request.method == 'GET':
-        return render_template('index.html', url_style=url_style, nickname=nickname, level=level, rating=rating)
+        return render_template('index.html', url_style=url_style, nickname=nickname,
+                               level=level, rating=rating)
 
 
 if __name__ == '__main__':
