0ec35ce1357326f37d39a6f9ea0a8a6a80a94fed
sovun1a <burindima324@gmail.com>
2022-02-10 15:52:46 +0300

    8

---
+++ Загрузка файла/application.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import url_for, request, render_template
+
+app = Flask(__name__)
+img = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def form_sample():
+    global img
+    url_style = url_for('static', filename='css/style.css')
+    if request.method == 'GET':
+        print(img)
+        return render_template('index.html', url_style=url_style, img=img)
+    elif request.method == 'POST':
+        f = request.files['file']
+        if f.filename:
+            f.save(f'static/img/{f.filename}')
+            img = url_for('static', filename=f'img/{f.filename}')
+        return render_template('index.html', url_style=url_style, img=img)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
